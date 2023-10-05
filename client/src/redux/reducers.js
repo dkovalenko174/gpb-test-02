2eb095c8cb020bf461487a5158a30e2dfb82af7e
Dmitriy <d.kovalenko174@gmail.com>
2023-10-05 13:51:12 +0500

    Fixed bug

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -55,7 +55,13 @@ const serviceDetailsReducer = (state = initialServiceDetailsState, action) => {
     case FETCH_SERVICE_DETAILS_REQUEST:
       return { ...state, loading: true, error: null };
     case FETCH_SERVICE_DETAILS_SUCCESS:
-      return { ...state, service: action.payload, loading: false };
+      return {
+        ...state,
+        service: action.payload,
+        loading: false,
+        error: null,
+        errorMessage: "",
+      };
     case FETCH_SERVICE_DETAILS_FAILURE:
       return {
         ...state,
